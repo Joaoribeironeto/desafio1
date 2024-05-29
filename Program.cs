3ad01f81e44c4f8cec18bace391753a2ff17e438
João Ribeiro <67529204+Joaoribeironeto@users.noreply.github.com>
2024-05-28 22:57:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+float a = 3.8f;
+
+float b = 8.2f;
+void OperacoesBasicas(){
+
+    float adicao = a + b;
+    Console.WriteLine($"\n Adição = {adicao} ");
+    
+    float subtracao = a - b;
+    Console.WriteLine($"\n Subtração = {subtracao} ");
+
+    float multiplicacao = a * b;
+    Console.WriteLine($"\n Multiplicação = {multiplicacao} ");
+
+    float divisao = a / b;
+    Console.WriteLine($"\n Divisão = {divisao} ");
+}
+
+OperacoesBasicas();
